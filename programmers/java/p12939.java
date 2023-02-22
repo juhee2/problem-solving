667cf370bd587b8acebb788c58648894c8afbba8
juhee2 <rnlakro3@gmail.com>
2023-02-23 00:50:51 +0900

    docs: p12939  문제 설명 추가

--- programmers/java/p12939.java
+++ programmers/java/p12939.java
@@ -1,3 +1,10 @@
+/*
+ * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/12939
+ * 제목: 최댓값과 최솟값
+ * 설명: 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+        예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+ */
+
 package programmers.java;
 
 class Solution {
@@ -7,18 +14,19 @@ class Solution {
         int max = 0, min = 0;
         for (int i = 0; i < numbers.length; i++) {
             int num = Integer.parseInt(numbers[i]);
-            
+
             if (i == 0) {
-                min = num; max = num;
+                min = num;
+                max = num;
                 continue;
             }
-            
+
             min = (min > num) ? num : min;
             max = (max < num) ? num : max;
         }
-        
+
         answer = min + " " + max;
-            
+
         return answer;
     }
 }
