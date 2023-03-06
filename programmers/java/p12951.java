af5fd2275cd66fdb67459cb4af2a61c540d5fc53
juhee2 <rnlakro3@gmail.com>
2023-03-07 00:24:14 +0900

    docs: 12951 자바 코멘트 작성

--- programmers/java/p12951.java
+++ programmers/java/p12951.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
+import java.util.Locale;
+
 /*
  * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/12951
  * 제목: JadenCase 문자열 만들기
  * 메모: 공백이 여러개 올수있다는 거 주의!! 문제 잘읽기
- * 원래는 공백으로 쪼개서 첫글자만 대문자로 바꾸고 붙이면 되겠지 하고 생각했었는데 공백이 여러개 올수 있으므로 해당 방법으로는 공백이 여러개 올때 처리를 할수가 없다..
+ * 원래는 공백으로 쪼개서 첫글자만 대문자로 바꾸고 붙이면 되겠지 하고 생각했었는데 공백이 맨 마지막에 오는경우 맨마지막의 공백은 제외된다!!!
  */
 public class p12951 {
     public static void main(String[] args) {
-        String s = "for      the last week";
+        String s = "for      the last week  ";
 //        String s = "   ";
-        solution(s);
+        System.out.println(solution(s));
     }
+    // 한 글자씩
     public static String solution(String s) {
         String answer="";
 
@@ -33,6 +37,22 @@ public class p12951 {
 
         return answer;
     }
+
+//    public static String solution(String s) {
+//        String answer = "";
+//
+//        String[] sArr = s.toLowerCase().split("\\s");
+////        System.out.println(Arrays.toString(sArr)); // [for, , , , , , the, last, week] 이렇게됨. 주어진 문자열 week 뒤에 공백이 두개 존재!
+//        for (int i = 0; i < sArr.length; i++) {
+//            if (sArr[i].equals("")) {
+//                answer += " ";
+//                continue;
+//            }
+//            answer += sArr[i].substring(0,1).toUpperCase() + sArr[i].substring(1) + ((i == (sArr.length -1)) ? "" : " ");
+//        }
+//
+//        return answer;
+//    }
 
 
 }
